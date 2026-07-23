d21cdba91ff962b34191fc7e4096b5d57a46768e
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2026-07-23 16:56:19 -0300

    alterando jenknsfile

--- src/main/java/config/Environment.java
+++ src/main/java/config/Environment.java
@@ -6,32 +6,33 @@ public final class Environment {
     }
 
     public static String getEmail() {
-        String email = System.getenv("API_USER");
+        String email = System.getenv("API_EMAIL");
 
         if (email == null || email.isBlank()) {
             throw new IllegalStateException(
-                    "A variável API_USER não foi encontrada."
+                    "Variável de ambiente API_EMAIL não encontrada."
             );
         }
 
-        System.out.println("API_USER recebida: SIM");
+        email = email.trim();
+
+        System.out.println("API_EMAIL recebida: SIM");
         System.out.println("Tamanho do email: " + email.length());
-        System.out.println("Email termina com @gmail.com: "
-                + email.endsWith("@gmail.com"));
+        System.out.println("Email possui @: " + email.contains("@"));
 
         return email;
     }
 
     public static String getSenha() {
-        String senha = System.getenv("API_USER_PSW");
+        String senha = System.getenv("API_SENHA");
 
         if (senha == null || senha.isBlank()) {
             throw new IllegalStateException(
-                    "A variável API_USER_PSW não foi encontrada."
+                    "Variável de ambiente API_SENHA não encontrada."
             );
         }
 
-        System.out.println("API_USER_PSW recebida: SIM");
+        System.out.println("API_SENHA recebida: SIM");
         System.out.println("Tamanho da senha: " + senha.length());
 
         return senha;
